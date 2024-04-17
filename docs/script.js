a2a4d52ca4e8f9f9164c6cf65275f2216eb4a946
DukeIsCreating <dukeofdelmar@gmail.com>
2024-04-16 22:02:22 -0400

    try again to disable github styles

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,13 @@
-for (var i = 0; i < document.styleSheets.length; ++i) {
-    var sheet = document.styleSheets[i];
-    if (sheet.href.includes("/assets/css/style.css?")) {
-        sheet.disabled = true;
-        sheet.parentNode.removeChild(sheet);
+window.onload = function () {
+    disableGithubStyles();
+};
+
+function disableGithubStyles() {
+    for (var i = 0; i < document.styleSheets.length; ++i) {
+        var sheet = document.styleSheets[i];
+        if (sheet.href.includes("/assets/css/style.css?")) {
+            sheet.disabled = true;
+            sheet.parentNode.removeChild(sheet);
+        }
     }
 }
