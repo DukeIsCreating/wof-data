3e655ef674faf3893c6b4b5590fb889ea1b24ca8
DukeIsCreating <dukeofdelmar@gmail.com>
2024-04-16 22:43:43 -0400

    Stop trying to find ways to thwart me javascript

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,7 @@ window.onload = function () {
 function disableGithubStyles() {
     for (var i = 0; i < document.styleSheets.length; ++i) {
         var sheet = document.styleSheets[i];
-        if (sheet.href.includes("/assets/css/style.css?")) {
+        if (sheet.href !== null && sheet.href.includes("/assets/css/style.css?")) {
             sheet.disabled = true;
             if (sheet.parentNode !== null && sheet.parentNode !== undefined) {
                 sheet.parentNode.removeChild(sheet);
