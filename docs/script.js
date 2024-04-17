28d2850fafd0bdb52d3e1ce0644e811634bc0ced
DukeIsCreating <dukeofdelmar@gmail.com>
2024-04-16 21:58:51 -0400

    try to disable github's markdown html template poo

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < document.styleSheets.length; ++i) {
+    var sheet = document.styleSheets[i];
+    if (sheet.href.includes("/assets/css/style.css?")) {
+        sheet.disabled = true;
+        sheet.parentNode.removeChild(sheet);
+    }
+}
